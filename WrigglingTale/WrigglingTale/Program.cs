aa87d1c9bea1ea1d141e55146602c2f8347d98bc
KrashDevelopment <116433214+KrashDevelopment@users.noreply.github.com>
2023-12-25 12:21:35 +0100

    Tested GDI

--- WrigglingTale/WrigglingTale/Program.cs
+++ WrigglingTale/WrigglingTale/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("jjk");
+﻿using SharpGDI;
+
+GDI.InitContext(0, 0, 800, 600);
+            
+GDI.Begin();
+GDI.DrawRectangle(0, 0, 100, 200, GDI.FromRgb(255, 255, 255));
+GDI.PrintText("Hello World!", 10, 100, 100, 10, GDI.FromRgb(255, 0, 0));
+        
+GDI.ReleaseContext();
